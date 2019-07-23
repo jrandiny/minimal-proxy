4bed4c6655dd80c31753a9ac87da16617587b16a
jrandiny <jrandiny@gmail.com>
2019-07-23 18:22:44 +0700

    Add blocklist

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
 
+const blocklistFile = JSON.parse(fs.readFileSync('blocklist.json'));
+const blocklistRegex = blocklistFile.map((val) => {
+  return new RegExp(val);
+})
+const blocklistTest = (url) => {
+  return blocklistRegex.some((val) => {
+    return val.test(url);
+  });
+}
 
 const server = http.createServer(async (user_req, user_res) => {
   console.log(user_req.url);
-  console.log(user_req.headers);
-  let proxy_req = http.request(user_req.url, (proxy_res) => {
-    user_res.writeHead(proxy_res.statusCode, proxy_res.headers);
+  const url = user_req.url;
 
-    proxy_res.pipe(user_res, {
+  if (blocklistTest(url)) {
+    user_res.writeHead(403);
+    user_res.write('403 Forbidden - URL blocked');
+    user_res.end();
+  } else {
+    let proxy_req = http.request(url, (proxy_res) => {
+      user_res.writeHead(proxy_res.statusCode, proxy_res.headers);
+
+      proxy_res.pipe(user_res, {
+        end: true
+      });
+    });
+
+    user_req.pipe(proxy_req, {
       end: true
     });
-  });
+  }
 
-  user_req.pipe(proxy_req, {
-    end: true
-  });
 });
 
 server.listen(port, (err) => {
